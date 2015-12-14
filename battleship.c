e603ea569728dcee959866b03fb3c923c839516d
Tyler Gantt <tgantt@localhost.localdomain>
2015-12-14 10:18:12 -0500

    two-dimensional array creates simple grid for game to be played on

--- battleship.c
+++ battleship.c
@@ -13,39 +13,53 @@ struct Ship {
 
 int main(void)
 {
+//use a struct to create the ships
 	struct Ship Patrol_Boat;
 	struct Ship Submarine;
 	struct Ship Cruiser;
 	struct Ship Destroyer;
 	struct Ship Battleship;
 	struct Ship Aircraft_Carrier;
-
-	//starting with patrol boat
+	
 	strcpy(Patrol_Boat.shipname, "Patrol Boat");
 	Patrol_Boat.shipsize = 2;
 
-	//now the submarine?
 	strcpy(Submarine.shipname, "Submarine");
 	Submarine.shipsize = 2;
 
-	//and so on
 	strcpy(Cruiser.shipname, "Cruiser");
 	Cruiser.shipsize = 3;
 
-	//and so forth
 	strcpy(Destroyer.shipname, "Destroyer");
 	Destroyer.shipsize = 3;
 
-	//until done
 	strcpy(Battleship.shipname, "Battleship");
 	Battleship.shipsize = 4;
 
-	//with each ship
 	strcpy(Aircraft_Carrier.shipname, "Aircraft Carrier");
 	Aircraft_Carrier.shipsize = 5;
 	
-	printf("Aircraft Carrier Stats:\nName: %s\nSize: %d\n", Aircraft_Carrier.shipname, Aircraft_Carrier.shipsize);
-	
-		
+	printf("Boat Statistics:\n\n");
+	printf("Boat 1: %s\nSize: %d\n\n", Patrol_Boat.shipname, Patrol_Boat.shipsize);
+	printf("Boat 2: %s\nSize: %d\n\n", Submarine.shipname, Submarine.shipsize);
+	printf("Boat 3: %s\nSize: %d\n\n", Cruiser.shipname, Cruiser.shipsize);
+	printf("Boat 4: %s\nSize: %d\n\n", Destroyer.shipname, Destroyer.shipsize);
+	printf("Boat 5: %s\nSize: %d\n\n", Battleship.shipname, Battleship.shipsize);
+	printf("Boat 6: %s\nSize: %d\n\n", Aircraft_Carrier.shipname, Aircraft_Carrier.shipsize);
+
+//create the grid on which the game is played via two-dimensional array
+
+	char battlegrid[10][10];
+	int onedim = 0;
+	int twodim = 0;
+
+	for(onedim = 0; onedim < 10; onedim++) {
+		for(twodim = 0; twodim < 10; twodim++) {
+			battlegrid[onedim][twodim] = '0';
+			printf("%c ", battlegrid[onedim][twodim]);
+		}
+		printf("\n");
+	}
+
 	return 0;
 }	
